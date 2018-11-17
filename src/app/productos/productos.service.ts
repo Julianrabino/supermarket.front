eecbba7eeff9fc14133e85e9ca7fa86042a1c27e
Julian Rabino <Julianrabino@gmail.com>
2018-11-17 02:36:27 -0300

    fix

--- src/app/productos/productos.service.ts
+++ src/app/productos/productos.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { BonitaCaseService } from '../bonita/case/bonita-case.service';
 import { ConfigService } from '../config/config.service';
 import { Producto } from './producto.model';
-import { SessionService } from '../storage/session.service';
+import { SessionService } from '../session/session.service';
 
 @Injectable({
   providedIn: 'root'
